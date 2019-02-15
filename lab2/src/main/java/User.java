3936b273c748473bb9e5ada9a14131c3a0f06fe4
fl <fl>
2019-02-15 14:22:24 +0300

    user fields

--- lab2/src/main/java/User.java
+++ lab2/src/main/java/User.java
@@ -1,2 +1,17 @@
+import java.util.Date;
+
 public class User {
+
+    private String email;
+    private String login;
+    private String password;
+    private Date created_at;
+    private Date updated_at;
+    private String fname;
+    private String name;
+    private String lname;
+    private Date birth_date;
+    private boolean is_blocked;
+    private String auth_key;
+
 }
